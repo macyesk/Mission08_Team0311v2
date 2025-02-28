a3e5b7b003eaa21fab0695171d6813719cf6b62c
Ben Haggard <21bhaggard@gmail.com>
2025-02-28 09:53:53 -0700

    init commit

---
+++ Mission08_Team0311v2/Views/Home/Quadrants.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission08_Team0311v2.Views.Home
+{
+    public class QuadrantsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
